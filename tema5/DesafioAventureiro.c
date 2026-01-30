6f597b7d927feb28ba6dfe6d67ec9daed90138d2
BrunoFantinato <brunao0113@gmail.com>
2026-01-29 21:40:23 -0300

    Refactor code structure for improved readability and maintainability

---
+++ tema5/DesafioAventureiro.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define TAM 10
+#define AGUA 0
+#define NAVIO 3
+
+    int main(){
+
+        int tabuleiro [TAM][TAM];
+
+        for(int i = 0; i < TAM; i++)
+        {
+            for(int j = 0; j < TAM; j++)
+            {
+                tabuleiro[i][j] = AGUA;
+            }
+        }
+
+        for (int k = 0; k < 3; k++)
+        {
+            tabuleiro[2][4 + k] = NAVIO;
+        }
+
+        for (int k = 0; k < 3; k++)
+        {
+            tabuleiro[5 + k][8] = NAVIO;
+        }
+
+        for (int k = 0; k < 3; k++)
+        {
+            tabuleiro[7 + k][2 - k] = NAVIO;
+        }
+
+        printf("\n BATALHA NAVAL\n\n");
+
+        printf("  0 1 2 3 4 5 6 7 8 9\n");
+
+        for (int i = 0; i < TAM; i++)
+        {
+            printf("%d ", i);
+            for(int j = 0; j < TAM; j++)
+            {
+                printf("%d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+
+        return 0;
+    }
---
+++ tema5/VetMatLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int vetor[5];
+
+    for (int i = 0; i < 5; i++) 
+    {
+        vetor[i] = i * 2;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Vetor[%d] = %d\n", i, vetor[i]);            
+    }
+
+    return 0;
+
+}
